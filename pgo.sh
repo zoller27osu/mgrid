b0f67728fcf2c6518aa2efd7c6c8aee899afadce
Michael Zoller <zoller.27@osu.edu>
2015-07-13 11:14:43 -0500

    Added x2p_graph.sh for graphing the output of x2p. Added command line argument to pgo.sh to allow its use with differently named 'timing' cases. Restored x2p.pbs walltime to its normal amount.

--- pgo.sh
+++ pgo.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 #rm pgo.gp
-#egrep "pgo|pgn0|pgn1|pgn2" timing.log.$1 > pgo$1.dat
-#touch pgo$1.dat
-if [ -z "${1}" ]; then
-    echo "Usage: $0 number_of_nodes"
+#egrep "pgo|pgn0|pgn1|pgn2" $1.log.$2 > pgo$2.dat
+#touch pgo$2.dat
+if [ -z "${2}" ]; then
+    echo "Usage: $0 case_name number_of_nodes"
     exit
 fi
 
-if [ ! -f "timing.log.$1" ] || ! grep -lq Done timing.log.$1 #grep -lq "Ctrl-C" timing.log.$1
+if [ ! -f "$1.log.$2" ] || ! grep -lq Done $1.log.$2 #grep -lq "Ctrl-C" $1.log.$2
 then
-    echo "'timing.log.$1' not found! Running timing test."
+    echo "'$1.log.$2' not found! Running $1."
     if [ -n "${PE_ENV}" ]; then
         #printf %0.f\\n 15.4
-        hours=$(echo $1 | awk '{print int((log($1)/log(2))+0.5);}')
+        hours=$(echo $2 | awk '{print int((log($1)/log(2))+0.5);}')
 	echo "$0: Calculated time needed: $hours hours."
-        nekq timing $1 $hours
+        nekq $1 $2 $hours
     else
-        nekbmpi timing $1
+        nekbmpi $1 $2
     fi
     until grep -l Done logfile
     do
@@ -27,15 +27,15 @@ fi
 
 for p in pgo #pgn0 pgn1 pgn2
 do
-    echo "Creating $p$1.dat"
-    >$p$1.dat
-    for ((n=1; n<$1; n++))
+    echo "Creating $p$2.dat"
+    >$p$2.dat
+    for ((n=1; n<$2; n++))
     do
-        #grep $p timing.log.$1 | awk -v n="$n" '$1 ~ n' > $p$1-$n.dat
-        grep $p timing.log.$1 | awk -v n="$n" '$1 ~ n' >> $p$1.dat
-        echo "" >> $p$1.dat
+        #grep $p $1.log.$2 | awk -v n="$n" '$2 ~ n' > $p$2-$n.dat
+        grep $p $1.log.$2 | awk -v n="$n" '$2 ~ n' >> $p$2.dat
+        echo "" >> $p$2.dat
     done
     
-    gnuplot -e "filename='$p$1.dat'" pgo.gp
+    gnuplot -e "filename='$p$2.dat'" pgo.gp
 done
-#head -n -1 pgo$1.dat > temp.txt ; mv temp.txt pgo$1.dat
+#head -n -1 pgo$2.dat > temp.txt ; mv temp.txt pgo$2.dat
---
+++ x2p_graph.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+ALTS=(J F)
+COLORS=(CC0000 00CC00 0000CC CC00CC 00CCCC CCCC00 000000 CC8000 808080)
+#echo "set linetype 2 lc rgb '#00C000' lw 1"
+
+#egrep "pgo|pgn0|pgn1|pgn2" $1.log.$2 > pgo$2.dat
+if [ -z "${1}" ]; then
+    echo "Usage: $0 pbs_jobid [output_extension]"
+    exit
+fi
+output=${2:-png}
+
+if [ ! -f "$1.out" ]; then
+    echo "$1.out not found! Aborting."
+    exit
+fi
+
+mkdir -p x2p_out
+> $1.gp
+echo "# Written by x2p.sh"						>> $1.gp
+echo "set terminal x11 enhanced font 'Arial,20' persist"		>> $1.gp
+echo "set output '$1.$output'"						>> $1.gp
+echo "set multiplot layout 1,1"						>> $1.gp
+echo ""									>> $1.gp
+echo "set style data lines"						>> $1.gp
+#echo "set style line lw 3"						>> $1.gp
+echo "set title 'Performance Improvment of Jacobi smoothing function'"	>> $1.gp
+echo "set logscale xy 10"						>> $1.gp
+for i in "${!COLORS[@]}"; do
+    echo "set style line ($i+1) lc rgb '#${COLORS[$i]}'"		>> $1.gp
+done
+echo ""									>> $1.gp
+
+#echo "Creating $1.dat"
+first=true
+for i in "${!ALTS[@]}"
+do
+    p=${ALTS[$i]}
+    echo "Creating $1-$p.dat"
+    > $1-$p.dat
+    #echo "Adding case $p."
+    #grep $p $1.log.$2 | awk -v n="$n" '$2 ~ n' > $p$2-$n.dat
+    grep error$p $1.out 					>> $1-$p.dat
+    #echo "" 							>> $1.dat
+    mv $1-$p.dat x2p_out/.    
+
+    if [ $first ]
+    then
+        echo -n "plot '$1-$p.dat' using 10:4 title '$p' ls ($i+1)"	>> $1.gp
+        first=""
+    else
+        echo ", \\"							>> $1.gp
+        echo -n "     '$1-$p.dat' using 10:4 title '$p' ls ($i+1)"	>> $1.gp
+    fi
+    #gnuplot -e "filename='$p$2.dat'" pgo.gp
+done
+
+# Remove the last (extraneous & error-causing) comma
+#sed -i '$s/,$//' $1.gp
+# End that last line
+echo ""									>> $1.gp
+echo ""									>> $1.gp
+echo "unset multiplot"							>> $1.gp
+
+mv $1.gp x2p_out/.
+cd x2p_out
+
+#gnuplot -e "filename='$1-$p.dat'" $1.gp
+echo ""
+echo "~~~ start $1.gp: ~~~"
+cat $1.gp
+echo "~~~~~ end $1.gp ~~~~"
+echo ""
+gnuplot $1.gp
+#head -n -1 pgo$2.dat > temp.txt ; mv temp.txt pgo$2.dat
